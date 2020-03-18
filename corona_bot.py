e096831a248282782d332e2a55de175e074127ae
Rachit Jain <rachit.jain2095@gmail.com>
2020-03-18 19:57:39 +0000

    code formatting + making headers short so slack msg is more readable

--- corona_bot.py
+++ corona_bot.py
@@ -4,25 +4,34 @@ from tabulate import tabulate
 from slack_client import slacker
 
 URL = 'https://www.mohfw.gov.in/'
-response = requests.get(URL).content
-soup = BeautifulSoup(response, 'html.parser')
+AVOID_HEADERS = ['Total Confirmed cases', '(', ')']
 
 extract_contents = lambda row: [x.text.replace('\n', '') for x in row]
-extract_ith_row = lambda i: extract_contents(soup.find_all('tr')[i].find_all('td')) 
 
-header = extract_contents(soup.tr.find_all('th'))
+def shorten_header(h):
+    for kw in AVOID_HEADERS:
+        h = h.replace(kw, '')
+    return h
 
-stats = []
-all_rows = soup.find_all('tr')
-for row in all_rows:
-    stat = extract_contents(row.find_all('td'))
-    if stat:
-        if len(stat) == 5:
-            # last row
-            stat = ['', *stat]
-        stats.append(stat)
 
-table = tabulate(stats, headers=header, tablefmt='psql')
-slack_text = f'Please find CoronaVirus Summary for India below:\n```{table}```'
+if __name__ == '__main__':
+    
+    response = requests.get(URL).content
+    soup = BeautifulSoup(response, 'html.parser')
+    header = extract_contents(soup.tr.find_all('th'))
+    short_header = [shorten_header(h) for h in header]
 
-slacker()(slack_text)
+    stats = []
+    all_rows = soup.find_all('tr')
+    for row in all_rows:
+        stat = extract_contents(row.find_all('td'))
+        if stat:
+            if len(stat) == 5:
+                # last row
+                stat = ['', *stat]
+            stats.append(stat)
+    
+    table = tabulate(stats, headers=short_header, tablefmt='psql')
+    slack_text = f'Please find CoronaVirus Summary for India below:\n```{table}```'
+    
+    slacker()(slack_text)
