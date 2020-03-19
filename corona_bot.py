59814e813f1313fa782a0e6609e99f3b4375281d
Rachit Jain <rachit.jain2095@gmail.com>
2020-03-19 06:16:11 +0000

    MVP: sends slack notfiication as soon as data changes upto (1 min latency)

--- corona_bot.py
+++ corona_bot.py
@@ -1,45 +1,79 @@
+import datetime
+import json
 import requests
 import argparse
+import logging
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 from slack_client import slacker
 
-URL = 'https://www.mohfw.gov.in/'
-AVOID_HEADERS = ['Total Confirmed cases', '(', ')']
+FORMAT = '[%(asctime)-15s] %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
+URL = 'https://www.mohfw.gov.in/'
+SHORT_HEADERS = ['Sno', 'State','In','Fr','Cd','Dt']
+FILE_NAME = 'corona_india_data.json'
 extract_contents = lambda row: [x.text.replace('\n', '') for x in row]
 
-def shorten_header(h):
-    for kw in AVOID_HEADERS:
-        h = h.replace(kw, '')
-    return h
 
+def save(x):
+    with open(FILE_NAME, 'w') as f:
+        json.dump(x, f)
+
+
+def load():
+    res = {}
+    with open(FILE_NAME, 'r') as f:
+        res = json.load(f)
+    return res
+    
 
 if __name__ == '__main__':
    
     parser  = argparse.ArgumentParser()
-    parser.add_argument('--states', default='haryana')
+    parser.add_argument('--states', default=',')
     args = parser.parse_args()
     interested_states = args.states.split(',')
     
-    response = requests.get(URL).content
-    soup = BeautifulSoup(response, 'html.parser')
-    header = extract_contents(soup.tr.find_all('th'))
-    short_header = [shorten_header(h) for h in header]
+    current_time = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
 
-    stats = []
-    all_rows = soup.find_all('tr')
-    for row in all_rows:
-        stat = extract_contents(row.find_all('td'))
-        if stat:
-            if len(stat) == 5:
-                # last row
-                stat = ['', *stat]
-                stats.append(stat)
-            if any([s.lower() in stat[1].lower() for s in interested_states]):
-                stats.append(stat)
-    
-    table = tabulate(stats, headers=short_header, tablefmt='psql')
-    slack_text = f'Please find CoronaVirus Summary for India below:\n```{table}```'
-    
-    slacker()(slack_text)
+    try:
+        response = requests.get(URL).content
+        soup = BeautifulSoup(response, 'html.parser')
+        header = extract_contents(soup.tr.find_all('th'))
+
+        stats = []
+        all_rows = soup.find_all('tr')
+        for row in all_rows:
+            stat = extract_contents(row.find_all('td'))
+            if stat:
+                if len(stat) == 5:
+                    # last row
+                    stat = ['', *stat]
+                    stats.append(stat)
+                if any([s.lower() in stat[1].lower() for s in interested_states]):
+                    stats.append(stat)
+        
+        past_data = load()
+        cur_data = {x[1]: {current_time: x[2:]} for x in stats}
+   
+        changed = False
+
+        for state in cur_data:
+            past = past_data[state]['latest']
+            cur = cur_data[state][current_time]
+            if past != cur:
+                changed = True
+                logging.warning(f'Change for {state}:\n{past}->{cur}')
+
+        # override the latest one now
+        for state in cur_data:
+            cur_data[state]['latest'] = cur_data[state][current_time]
+        save(cur_data)
+
+        if changed:
+            table = tabulate(stats, headers=SHORT_HEADERS, tablefmt='psql')
+            slack_text = f'Please find CoronaVirus Summary for India below:\n```{table}```'
+            slacker()(slack_text)
+    except Exception as e:
+        slacker()(f'Exception occured: [{e}]')
